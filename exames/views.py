4386eac6536d68d047b1d92ff002da751eb263c9
leonsaja <leonardosaja87@gamil.com>
2023-10-06 07:03:17 -0300

    solicitar exames concluido

--- exames/views.py
+++ exames/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from .models import TiposExames
+from .models import TiposExames,PedidosExames,SolicitacaoExame
+from datetime import datetime
+from django.contrib import messages
+from django.contrib.messages import constants
+
 def solicitar_exames(request):
     
     tiposexames=TiposExames.objects.all()
@@ -19,4 +23,28 @@ def solicitar_exames(request):
            
         print('total',total) 
         
-        return render(request,'solicitar_exames.html',{"tiposexames":tiposexames,'solicitacao':solicitar_exames,'total':total})           
+        return render(request,'solicitar_exames.html',{"tiposexames":tiposexames,'solicitacao':solicitar_exames,'total':total})
+    
+def fechar_pedido(request):    
+    exames_id=request.POST.getlist('exames') 
+    solicitacao_exames=TiposExames.objects.filter(id__in=exames_id)
+    
+    pedido_exames=PedidosExames(
+        usuario=request.user,
+        data=datetime.now()
+    )
+    pedido_exames.save()
+    
+    for exame in solicitacao_exames:
+        solicitacao_exames_temp=SolicitacaoExame(
+            usuario=request.user,
+            exame=exame,
+            status='E',
+             
+        )
+        solicitacao_exames_temp.save()
+        pedido_exames.exames.add(solicitacao_exames_temp)
+    pedido_exames.save()
+    
+    messages.add_message(request, constants.SUCCESS, 'Pedido de exame concluído com sucesso')
+    return redirect('ver_pedidos')
