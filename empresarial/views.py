a88dbb3d086746c3afc24babe9d6f9b65851a0e3
leonsaja <leonardosaja87@gamil.com>
2023-10-08 11:40:07 -0300

    gerenciar_cliente

---
+++ empresarial/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.db.models import Value
+from django.db.models.functions import Concat
+from django.contrib.admin.views.decorators import staff_member_required
+from exames.models import SolicitacaoExame
+from django.shortcuts import get_object_or_404
+staff_member_required
+def gerenciar_clientes(request):
+    clientes=User.objects.filter(is_staff=False)
+    nome_completo = request.GET.get('nome')
+    email = request.GET.get('email')
+
+    if email:
+        clientes = clientes.filter(email__contains = email)
+    if nome_completo:
+        print('nome_completo',nome_completo)
+        clientes = clientes.annotate(
+            full_name=Concat('first_name', Value(' '), 'last_name')
+        ).filter(full_name__contains=nome_completo)
+   
+    return render(request,'gerenciar_cliente.html',{'clientes':clientes})
+
+def cliente(request,cliente_id): 
+    cliente=get_object_or_404(User,id=cliente_id)
+    exames=SolicitacaoExame.objects.filter(usuario=cliente)
+    return render(request,'cliente.html',{'cliente':cliente,'exames':exames})
--- vitalab/settings.py
+++ vitalab/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'usuarios',
     'exames',
+    'empresarial'
 ]
 
 MIDDLEWARE = [
