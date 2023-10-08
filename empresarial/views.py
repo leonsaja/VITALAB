5e7413ab58bc375a66632fa7fe9c5294ac177b8e
leonsaja <leonardosaja87@gamil.com>
2023-10-08 13:12:31 -0300

    gerenciar exames dos cliente

--- empresarial/views.py
+++ empresarial/views.py
@@ -1,3 +1,4 @@
+from django.http import FileResponse
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.db.models import Value
@@ -20,8 +21,20 @@ def gerenciar_clientes(request):
         ).filter(full_name__contains=nome_completo)
    
     return render(request,'gerenciar_cliente.html',{'clientes':clientes})
-
+staff_member_required
 def cliente(request,cliente_id): 
     cliente=get_object_or_404(User,id=cliente_id)
     exames=SolicitacaoExame.objects.filter(usuario=cliente)
     return render(request,'cliente.html',{'cliente':cliente,'exames':exames})
+
+staff_member_required
+def exame_cliente(request, exame_id):
+    exame = SolicitacaoExame.objects.get(id=exame_id)
+    return render(request, 'exame_cliente.html', {'exame': exame})
+
+@staff_member_required 
+def proxy_pdf(request, exame_id):
+    exame = SolicitacaoExame.objects.get(id=exame_id)
+    print(exame)
+    response = exame.resultado.open()
+    return FileResponse(response)
